fdaf14dc3faa97065ce874a65565578880300f37
VadimOVB <64534501+VadimOVB@users.noreply.github.com>
2022-01-03 11:26:45 +0300

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,43 @@
 namespace SpriteKind {
     export const Camera = SpriteKind.create()
+    export const invent = SpriteKind.create()
+    export const StopEnemy = SpriteKind.create()
 }
+let invDoor: tiles.Location[] = []
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(mySpritePic[0])
+})
 scene.onHitWall(SpriteKind.Player, function (sprite, location) {
-    if (tiles.tileAtLocationEquals(location, assets.tile`myTile`) && controller.B.isPressed()) {
-        tiles.setTileAt(location, assets.tile`myTile0`)
-        tiles.setWallAt(location, false)
+    if (controller.A.isPressed()) {
+        if (tiles.tileAtLocationEquals(location, assets.tile`myTile`)) {
+            tiles.setTileAt(location, assets.tile`myTile0`)
+            tiles.setWallAt(location, false)
+        } else if (tiles.tileAtLocationEquals(location, assets.tile`myTile13`)) {
+            tiles.setTileAt(location, assets.tile`myTile14`)
+            tiles.setWallAt(location, false)
+        } else if (tiles.tileAtLocationEquals(location, assets.tile`myTile15`)) {
+            tiles.setTileAt(location, assets.tile`myTile5`)
+            rubilnikiON()
+        } else if (tiles.tileAtLocationEquals(location, assets.tile`myTile16`)) {
+            tiles.setTileAt(location, assets.tile`myTile6`)
+            rubilnikiON()
+        } else if (tiles.tileAtLocationEquals(location, assets.tile`myTile17`)) {
+            tiles.setTileAt(location, assets.tile`myTile8`)
+            rubilnikiON()
+        } else if (tiles.tileAtLocationEquals(location, sprites.dungeon.greenSwitchDown)) {
+            tiles.setTileAt(location, assets.tile`myTile7`)
+            rubilnikiON()
+        }
     }
 })
-controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mySprite.tileKindAt(TileDirection.Center, assets.tile`myTile2`)) {
+        numCam = 0
         for (let value of sprites.allOfKind(SpriteKind.Camera)) {
+            numCam += 1
             lantern.startLanternEffect(value)
             scene.cameraFollowSprite(value)
+            value.sayText(numCam)
             pause(1500)
         }
         if (light2) {
@@ -20,19 +46,98 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
             lantern.startLanternEffect(mySprite)
         }
         scene.cameraFollowSprite(mySprite)
+    } else if (tiles.tileAtLocationEquals(tiles.getTileLocation(Math.trunc(mySprite.x / 16), Math.trunc(mySprite.y / 16) + 1), assets.tile`myTile0`) && mySprite.image == mySpritePic[2]) {
+        tiles.setTileAt(tiles.getTileLocation(Math.trunc(mySprite.x / 16), Math.trunc(mySprite.y / 16) + 1), assets.tile`myTile`)
+        tiles.setWallAt(tiles.getTileLocation(Math.trunc(mySprite.x / 16), Math.trunc(mySprite.y / 16) + 1), true)
+    } else if (tiles.tileAtLocationEquals(tiles.getTileLocation(Math.trunc(mySprite.x / 16), Math.trunc(mySprite.y / 16) - 1), assets.tile`myTile0`) && mySprite.image == mySpritePic[0]) {
+        tiles.setTileAt(tiles.getTileLocation(Math.trunc(mySprite.x / 16), Math.trunc(mySprite.y / 16) - 1), assets.tile`myTile`)
+        tiles.setWallAt(tiles.getTileLocation(Math.trunc(mySprite.x / 16), Math.trunc(mySprite.y / 16) - 1), true)
+    } else if (tiles.tileAtLocationEquals(tiles.getTileLocation(Math.trunc(mySprite.x / 16) + 1, Math.trunc(mySprite.y / 16)), assets.tile`myTile14`) && mySprite.image == mySpritePic[1]) {
+        tiles.setTileAt(tiles.getTileLocation(Math.trunc(mySprite.x / 16) + 1, Math.trunc(mySprite.y / 16)), assets.tile`myTile13`)
+        tiles.setWallAt(tiles.getTileLocation(Math.trunc(mySprite.x / 16) + 1, Math.trunc(mySprite.y / 16)), true)
+    } else if (tiles.tileAtLocationEquals(tiles.getTileLocation(Math.trunc(mySprite.x / 16) - 1, Math.trunc(mySprite.y / 16)), assets.tile`myTile14`) && mySprite.image == mySpritePic[3]) {
+        tiles.setTileAt(tiles.getTileLocation(Math.trunc(mySprite.x / 16) - 1, Math.trunc(mySprite.y / 16)), assets.tile`myTile13`)
+        tiles.setWallAt(tiles.getTileLocation(Math.trunc(mySprite.x / 16) - 1, Math.trunc(mySprite.y / 16)), true)
     }
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile4`, function (sprite, location) {
-    tiles.setTileAt(location, assets.tile`transparency16`)
+    tiles.setTileAt(location, assets.tile`myTile18`)
     lantern.startLanternEffect(sprite)
     light2 = false
+    for (let value of tiles.getTilesByType(assets.tile`myTile5`)) {
+        tiles.setTileAt(value, assets.tile`myTile15`)
+    }
+    for (let value of tiles.getTilesByType(assets.tile`myTile6`)) {
+        tiles.setTileAt(value, assets.tile`myTile16`)
+    }
+    for (let value of tiles.getTilesByType(assets.tile`myTile7`)) {
+        tiles.setTileAt(value, sprites.dungeon.greenSwitchDown)
+    }
+    for (let value of tiles.getTilesByType(assets.tile`myTile8`)) {
+        tiles.setTileAt(value, assets.tile`myTile17`)
+    }
 })
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(mySpritePic[3])
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(mySpritePic[1])
+})
+function rubilnikiON () {
+    if (tiles.getTilesByType(assets.tile`myTile15`).length + tiles.getTilesByType(assets.tile`myTile16`).length + tiles.getTilesByType(assets.tile`myTile17`).length + tiles.getTilesByType(sprites.dungeon.greenSwitchDown).length == 0) {
+        light2 = true
+        lantern.stopLanternEffect()
+        for (let value of tiles.getTilesByType(assets.tile`myTile18`)) {
+            tiles.setTileAt(value, assets.tile`myTile4`)
+        }
+    }
+}
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(mySpritePic[2])
+})
+let numCam = 0
 let mySprite2: Sprite = null
 let mySprite: Sprite = null
+let mySpritePic: Image[] = []
 let light2 = false
 lantern.setBreathingEnabled(true)
 light2 = true
-mySprite = sprites.create(img`
+mySpritePic = [
+img`
+    . . f f f f f f f f . . 
+    . f e e e e e e e f f . 
+    f f e f e e e e e e f f 
+    f f f e e e e e e e e f 
+    f f f e e e e e e f e f 
+    f f f f e e e e f f f f 
+    f f f f f f f f f f f f 
+    f f f f f f f f f f f f 
+    . f f f f f f f f f f . 
+    . e f f f f f f f f e . 
+    e 4 f b b b b b b f 4 e 
+    4 d f d d d d d d c d 4 
+    4 4 f 6 6 6 6 6 6 f 4 4 
+    . . . f f f f f f . . . 
+    . . . f f . . f f . . . 
+    `,
+img`
+    . . . f f f f f f . . . 
+    . f f f e e e e e f . . 
+    f f f e e e e e e e f . 
+    f f f f e e e e e e e f 
+    f f f f f e e e 4 e e f 
+    f f f f e e e 4 4 e e f 
+    f f f f 4 4 4 4 4 e f f 
+    f f 4 e 4 f f 4 4 e f . 
+    . f 4 d 4 d d d d f . . 
+    . f f f e e d b b f . . 
+    . . f 4 d d e 4 e f . . 
+    . . f e d d e 1 1 f . . 
+    . f f f e e f 6 6 f f . 
+    . f f f f f f f f f f . 
+    . . f f . . . f f f . . 
+    `,
+img`
     . . . f f f f f f . . . 
     . f f e e e e e e f f . 
     f f f f 4 e e e f f f f 
@@ -48,12 +153,134 @@ mySprite = sprites.create(img`
     4 4 f 6 6 6 6 6 6 f 4 4 
     . . . f f f f f f . . . 
     . . . f f . . f f . . . 
-    `, SpriteKind.Player)
+    `,
+img`
+    . . . f f f f f f . . . 
+    . . f e e e e e f f f . 
+    . f e e e e e e e f f f 
+    f e e e e e e e f f f f 
+    f e e 4 e e e f f f f f 
+    f e e 4 4 e e e f f f f 
+    f f e 4 4 4 4 4 f f f f 
+    . f e 4 4 f f 4 e 4 f f 
+    . . f d d d d 4 d 4 f . 
+    . . f b b d e e f f f . 
+    . . f e 4 e d d 4 f . . 
+    . . f 1 1 e d d e f . . 
+    . f f 6 6 f e e f f f . 
+    . f f f f f f f f f f . 
+    . . f f f . . . f f . . 
+    `
+]
+let myEnemy = [
+sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . c c . . . 
+    . . . . . . . c c c c 6 3 c . . 
+    . . . . . . c 6 3 3 3 3 6 c . . 
+    . . . . . c 6 6 3 3 3 3 3 3 c . 
+    . . . . c 6 6 6 6 3 3 3 3 3 3 c 
+    . c c c c c 6 6 c c 3 3 3 3 3 c 
+    b 5 5 c 3 3 c c 5 5 c 3 3 3 c c 
+    f f 5 c c c 3 c 5 f f 6 6 6 c c 
+    f f 5 c c c c c 5 f f 3 3 3 3 c 
+    . b 5 5 3 c 3 5 5 c 3 3 3 3 3 c 
+    . c 4 4 5 5 5 5 4 c c 3 3 3 c . 
+    c 4 5 5 4 4 4 4 5 5 4 c b b . . 
+    c 5 5 5 c 4 c 5 5 5 c 4 c 5 c . 
+    c 5 5 5 5 c 5 5 5 5 c 4 c 5 c . 
+    . c c c c c c c c c . . c c c . 
+    `, SpriteKind.StopEnemy),
+sprites.create(img`
+    . . f f f . . . . . . . . f f f 
+    . f f c c . . . . . . f c b b c 
+    f f c c . . . . . . f c b b c . 
+    f c f c . . . . . . f b c c c . 
+    f f f c c . c c . f c b b c c . 
+    f f c 3 c c 3 c c f b c b b c . 
+    f f b 3 b c 3 b c f b c c b c . 
+    . c b b b b b b c b b c c c . . 
+    . c 1 b b b 1 b b c c c c . . . 
+    c b b b b b b b b b c c . . . . 
+    c b c b b b c b b b b f . . . . 
+    f b 1 f f f 1 b b b b f c . . . 
+    f b b b b b b b b b b f c c . . 
+    . f b b b b b b b b c f . . . . 
+    . . f b b b b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    `, SpriteKind.StopEnemy),
+sprites.create(img`
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f . . . . . 
+    . c d f d d f d e e f f . . . . 
+    . c d f d d f d e e d d f . . . 
+    c d e e d d d d e e b d c . . . 
+    c d d d d c d d e e b d c . f f 
+    c c c c c d d d e e f c . f e f 
+    . f d d d d d e e f f . . f e f 
+    . . f f f f f e e e e f . f e f 
+    . . . . f e e e e e e e f f e f 
+    . . . f e f f e f e e e e f f . 
+    . . . f e f f e f e e e e f . . 
+    . . . f d b f d b f f e f . . . 
+    . . . f d d c d d b b d f . . . 
+    . . . . f f f f f f f f f . . . 
+    `, SpriteKind.StopEnemy),
+sprites.create(img`
+    . . . . . c c c c c c c . . . . 
+    . . . . c 6 7 7 7 7 7 6 c . . . 
+    . . . c 7 c 6 6 6 6 c 7 6 c . . 
+    . . c 6 7 6 f 6 6 f 6 7 7 c . . 
+    . . c 7 7 7 7 7 7 7 7 7 7 c . . 
+    . . f 7 8 1 f f 1 6 7 7 7 f . . 
+    . . f 6 f 1 f f 1 f 7 7 7 f . . 
+    . . . f f 2 2 2 2 f 7 7 6 f . . 
+    . . c c f 2 2 2 2 7 7 6 f c . . 
+    . c 7 7 7 7 7 7 7 7 c c 7 7 c . 
+    c 7 1 1 1 7 7 7 7 f c 6 7 7 7 c 
+    f 1 1 1 1 1 7 6 f c c 6 6 6 c c 
+    f 1 1 1 1 1 1 6 6 c 6 6 6 c . . 
+    f 6 1 1 1 1 1 6 6 6 6 6 6 c . . 
+    . f 6 1 1 1 1 1 6 6 6 6 c . . . 
+    . . f f c c c c c c c c . . . . 
+    `, SpriteKind.StopEnemy)
+]
+for (let value of sprites.allOfKind(SpriteKind.StopEnemy)) {
+    tiles.placeOnRandomTile(value, sprites.dungeon.buttonOrange)
+    tiles.setTileAt(tiles.getTileLocation(Math.trunc(value.x / 16), Math.trunc(value.y / 16)), sprites.dungeon.buttonOrangeDepressed)
+}
+mySprite = sprites.create(mySpritePic[2], SpriteKind.Player)
 controller.moveSprite(mySprite)
 scene.setBackgroundColor(11)
 tiles.setTilemap(tilemap`level1`)
 tiles.placeOnRandomTile(mySprite, assets.tile`myTile1`)
 scene.cameraFollowSprite(mySprite)
+let item = sprites.create(img`
+    3 . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.invent)
+for (let value of invDoor) {
+    let doorY: number[] = []
+    let doorX: number[] = []
+    tiles.placeOnTile(item, value)
+    doorX.push(Math.floor(item.x / 16))
+    doorY.push(Math.floor(item.y / 16))
+}
 for (let value of tiles.getTilesByType(assets.tile`myTile3`)) {
     mySprite2 = sprites.create(img`
         . . . . . b c . . . . . . . . . 
@@ -76,5 +303,5 @@ for (let value of tiles.getTilesByType(assets.tile`myTile3`)) {
     tiles.placeOnTile(mySprite2, value)
 }
 game.onUpdateInterval(500, function () {
-    mySprite.sayText("" + convertToText(Math.floor(mySprite.x)) + ":" + convertToText(Math.round(mySprite.y)))
+    mySprite.sayText("" + convertToText(Math.trunc(mySprite.x / 16)) + ":" + convertToText(Math.trunc(mySprite.y / 16)) + " " + Math.trunc(mySprite.x) + ":" + Math.trunc(mySprite.y))
 })
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -3,10 +3,6 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
-    export const tile1 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile2 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
     export const tile3 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile4 = image.ofBuffer(hex``);
@@ -14,14 +10,46 @@ namespace myTiles {
     export const tile5 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile14 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile13 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile15 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile17 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile18 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile19 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile20 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "level1":
-            case "level1":return tiles.createTilemap(hex`1900190008080808080808080101010101010101010101010101010102080b000000000d0800000a00000000000000000000000009030800000000000008000008000000000000000008080000000308000000000000080000080000000000000000080d00000003080000000000000800000800000008000000000800000000030808080808080a080000080000000800000000080000000003070d000000000000000008080808080808000008000000000307000000000000000000080d0000000000000008000000000307000000080808080808080000000000000000080a08080803070000000000000000000000000000000000000000000000030700000000000000000000000000000000000000000000000307000808080808080a0808080808080000080808080808080307000809000d000000080c00000d0800000000000d08090003070008000808080000080000000e0800000000000008000003070008000000000000080808080a080000080808080800000307000800000000000000000000000000000000000000000003070000000000000000000000000000000000000000000000030700000008080808080808080800000808080808080800000307000000000009080d00000000000000000009080d0000000307000000000000080000000000000000000000080000000003070008000000000800000000000000000000000800000000030700080000000008080000000008080808080a0800000000030700080000000000000000000000000000000000000000000307000a0000000000000000000000000000000000000000000306050505050505050505050505050505050505050505050504`, img`
+            case "level1":return tiles.createTilemap(hex`1900190014010101011601140101140101120101010112010112160102070a000000000c0700001c0000000000000000000000001d030700000000000007000007000000000000000014011b00000307000000000000070000070000000000000000070c000000030700000000000007000007000000190000000007000000000314010101010109150000071d00000700000000070000000003070c00000000000000001401010101011b000007000000000307000000000000000000070c00000000000000070000000013070000001801010101011500000000000000001809010101020700000000000000000000000000000000000000000000000307000000000000000000000000000000000000000000000003070014010101010109140101010119000018010101140101020700071d000c000000070b00000c0700000000000c071d00031100070018011b0000070000000d070000000000000700000e0700070000000000001801010109150000180101011500000307001a00000000000000000000000000000000000000000003110000000000000000000000000000000000000000000000130700000018010114010101011b00001801010114011b0000030700000000001d070c0000000000000000001d070c000000030700000000000007000000000000000000000007000000000307001900000000071d000000000000000000000700000000030700070000000018011b0000001801010101091500000000131700070000000000000000000000000000000000000000000307001c00000000000000000000000000000000000000000803060506100505051005050505100505050505051005050f1004`, img`
 2222222222222222222222222
 2......2..2.............2
-2......2..2........22...2
+2......2..2........222..2
 2......2..2........2....2
 2......2..2...2....2....2
 22222222..2...2....2....2
@@ -40,11 +68,49 @@ namespace myTiles {
 2......2...........2....2
 2......2...........2....2
 2.2....2...........2....2
-2.2....22....2222222....2
+2.2....222...2222222....2
 2.2.....................2
 2.2.....................2
 2222222222222222222222222
-`, [myTiles.transparency16,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterSouth1,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterWest0,sprites.builtin.brick,sprites.dungeon.buttonOrangeDepressed,myTiles.tile1,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6], TileScale.Sixteen);
+`, [myTiles.transparency16,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterSouth1,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterWest0,sprites.dungeon.buttonOrangeDepressed,myTiles.tile1,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterNorth1,sprites.dungeon.greenOuterEast1,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenInnerSouthEast,myTiles.tile9,myTiles.tile10,myTiles.tile14,myTiles.tile11,myTiles.tile12,myTiles.tile13,myTiles.tile15,sprites.dungeon.buttonOrange], TileScale.Sixteen);
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+            case "level3":
+            case "level3":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
         }
         return null;
     })
@@ -52,10 +118,6 @@ namespace myTiles {
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
-            case "myTile":
-            case "tile1":return tile1;
-            case "myTile0":
-            case "tile2":return tile2;
             case "myTile1":
             case "tile3":return tile3;
             case "myTile2":
@@ -64,6 +126,38 @@ namespace myTiles {
             case "tile5":return tile5;
             case "myTile4":
             case "tile6":return tile6;
+            case "myTile5":
+            case "tile7":return tile7;
+            case "myTile6":
+            case "tile8":return tile8;
+            case "myTile7":
+            case "tile9":return tile9;
+            case "myTile8":
+            case "tile10":return tile10;
+            case "myTile12":
+            case "tile14":return tile14;
+            case "myTile9":
+            case "tile11":return tile11;
+            case "myTile10":
+            case "tile12":return tile12;
+            case "myTile11":
+            case "tile13":return tile13;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile13":
+            case "tile15":return tile15;
+            case "myTile14":
+            case "tile16":return tile16;
+            case "myTile15":
+            case "tile17":return tile17;
+            case "myTile16":
+            case "tile18":return tile18;
+            case "myTile17":
+            case "tile19":return tile19;
+            case "myTile18":
+            case "tile20":return tile20;
         }
         return null;
     })
